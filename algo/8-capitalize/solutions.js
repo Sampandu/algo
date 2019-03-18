d687d86228769042bc4afc92efd5160371a4b9b2
Du <Sampan0103314@gmail.com>
2019-03-17 23:44:57 -0400

    8-capitalize

---
+++ algo/8-capitalize/solutions.js
@@ -0,0 +1,20 @@
+//solution1
+const capitalize = str => {
+  return str.split(' ')
+            .map(word => word[0]
+            .toUpperCase() + word.slice(1))
+            .join(' ')
+}
+
+//solution2
+const capitalize = str => {
+  let result = ''
+  for (let i = 0; i < str.length; i++) {
+    i === 0 || str[i-1] === ' '
+      ? result += str[i].toUpperCase()
+      : result += str[i]
+  }
+
+
+  return result
+}
