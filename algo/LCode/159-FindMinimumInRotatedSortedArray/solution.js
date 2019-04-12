5352135f5430b39e6d94a36241f328708aeec4a2
Du <Sampan0103314@gmail.com>
2019-04-12 10:24:38 -0400

    solution 2 for 159-FindMinmumInRotatedSortedArray

--- algo/LCode/159-FindMinimumInRotatedSortedArray/solution.js
+++ algo/LCode/159-FindMinimumInRotatedSortedArray/solution.js
@@ -1,3 +1,4 @@
+//solution1
 const findMin = function (nums) {
   if(nums.length === 0) return
 
@@ -16,4 +17,22 @@ const findMin = function (nums) {
 
   if(nums[start] < nums[end]) return nums[start]
   else return nums[end]
+}
+
+//solution2
+const findMin = nums => {
+  if(nums.lenght === 0) return
+
+  let start = 0, end = nums.length - 1,
+      target = nums[nums.length - 1]
+  while(start + 1 < end) {
+      const mid = start + Math.floor((end - start) / 2)
+      if(nums[mid] < target) {
+          end = mid
+      } else {
+          start = mid
+      }
+  }
+
+  return Math.min(nums[start], nums[end])
 }
