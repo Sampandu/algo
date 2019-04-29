9217c7d86cf5433da295c2e6bc6667e19b3dc5d1
Du <Sampan0103314@gmail.com>
2019-04-29 10:55:20 -0400

    optimize solution using Rabin Karp algo

--- LeetCode/28-ImplementStrStr/solution.js
+++ LeetCode/28-ImplementStrStr/solution.js
@@ -1,29 +1,10 @@
+/* eslint-disable complexity */
+/* eslint-disable no-empty */
+/* eslint-disable no-unused-vars */
+/* eslint-disable semi */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-multiple-empty-lines */
 //solution1, time complexity O(n^2)
-var strStr = function(haystack, needle) {
-  if (needle === '') {
-      return 0
-  }
-
-  let start = -1
-
-  for (let i = 0; i < haystack.length; i++) {
-      if (haystack[i] === needle[0]) {
-          let j = 1
-          for (; j < needle.length; j++) {
-              if (needle[j] !== haystack[i + j]) {
-                  break
-              }
-          }
-          if (j === needle.length) {
-              start = i
-              break
-          }
-      }
-  }
-  return start
-};
-
-//optimized version
 var strStr = function(haystack, needle) {
     if (needle === '') {
         return 0
@@ -46,3 +27,58 @@ var strStr = function(haystack, needle) {
     }
     return -1
 };
+
+//solution2, using Rabin Karp algo, time complexity O(n+m)
+var strStr = function(haystack, needle) {
+    const BASE = 1000000   //module base
+
+    const m = needle.length
+    if (m === 0) {
+        return 0
+    }
+
+    if (haystack ==='') {
+        return -1
+    }
+
+    //calculate 31^m, 31 is a experienced coefficient in constructing a hash function
+    let power = 1
+    for (let i = 0; i < m; i++) {
+        power = power * 31 % BASE
+    }
+
+    //calculate hashing value of needle
+    let needleCode = 0
+    for (let i = 0; i < m; i++) {
+        //hash function calculating the integer of a substring
+        //use charCodeAt(i) to get the ASCII value
+        needleCode = (needleCode * 31 + needle.charCodeAt(i)) % BASE
+    }
+
+    //calculate the hashing value of substring of haystack
+    let haystackCode = 0
+    for (let i = 0;  i < haystack.length; i++) {
+        haystackCode = (haystackCode * 31 + haystack.charCodeAt(i)) % BASE
+
+        //abc + d
+        if (i < m - 1) {
+            continue
+        }
+
+        //abc + d - a
+        if (i >= m) {
+            haystackCode = haystackCode - (haystack.charCodeAt(i - m) * power) % BASE
+            if (haystackCode < 0) {
+                haystackCode += BASE
+            }
+        }
+
+        if (needleCode === haystackCode) {
+            //slice(start, end) rather than slice(start, length)
+            if (haystack.slice(i - m + 1, i + 1) === needle) {
+                return i - m + 1
+            }
+        }
+    }
+    return -1
+}
