4b15ac1820c556f3107eac1020c58f1619f53af5
Du <Sampan0103314@gmail.com>
2019-05-08 10:28:35 -0400

    LCode 8

---
+++ LCode/8-RotateString/solution.js
@@ -0,0 +1,19 @@
+const rotateString = (str, offset) => {
+  //if offset is larger than the length of the string, use module %
+  offset = offset % str.length
+  str = reverse(str, 0, str.length - 1)
+  str = reverse(str, 0, offset - 1)
+  str = reverse(str, offset, str.length - 1)
+  return str
+}
+
+const reverse = (str, start, end) => {
+  //elements in string can not be swop with each other
+  str = str.split('')
+  for (let i = start, j = end; i < j; i++, j--) {
+    const temp = str[i]
+    str[i] = str[j]
+    str[j] = temp
+  }
+  return str.join('')
+}
