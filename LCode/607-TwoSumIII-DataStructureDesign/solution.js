57e6d399d0e220243e6e90da767dcb9eb574941c
Du <Sampan0103314@gmail.com>
2019-05-16 11:02:35 -0400

    update LCode 607

--- LCode/607-TwoSumIII-DataStructureDesign/solution.js
+++ LCode/607-TwoSumIII-DataStructureDesign/solution.js
@@ -1,3 +1,4 @@
+//solution1
 const TwoSum = function() {
   this.hashMap = new Map()
 }
@@ -20,4 +21,31 @@ TwoSum.prototype.find = function(value) {
     }
   }
   return false
+}
+
+//solution2
+class TwoSum {
+  constructor() {
+    this.hashMap = {}
+  }
+
+  add(number) {
+    this.hashMap[number] = this.hashMap[number] || 0
+    this.hashMap[number]++
+  }
+
+  find(value) {
+    for (let key in this.hashMap) {
+      const diff = value - parseInt(key)
+
+      if (diff === key) {
+        if (this.hashMap[diff] >= 2) {
+          return true
+        }
+      } else if (this.hashMap[diff] >= 1) {
+        return true
+      }
+    }
+    return false
+  }
 }
