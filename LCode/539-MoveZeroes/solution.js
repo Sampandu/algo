1194bbea6e4ffd51b5c2d9c3e02f0ffc057d0130
Du <Sampan0103314@gmail.com>
2019-05-19 22:16:46 -0400

    update solution to LCode 539

--- LCode/539-MoveZeroes/solution.js
+++ LCode/539-MoveZeroes/solution.js
@@ -1,20 +1,45 @@
-//solution, move zero to the right === move non-zero numbers to the left
+//solution1, move zero to the right === move non-zero numbers to the left
 const moveZeroes = nums => {
   // write your code here
   if (nums === null || nums.length === 0) {
       return null
   }
 
-  let j = 0
-  for (let i = 0; i < nums.length; i++) {
-      if (nums[i] === 0) {
-          continue
+  let left = 0,
+      right = 0
+  while (right < nums.length) {
+      if (nums[right] !== 0) {
+          const temp = nums[right]
+          nums[right] = nums[left]
+          nums[left] = temp
+          left++
       }
-
-      const temp = nums[i]
-      nums[i] = nums[j]
-      nums[j] = temp
-      j++
+      right++
   }
   return nums
+}
+
+//solution2, mininum writing to the array
+const moveZeroes = nums => {
+    if (nums === null || nums.length === 0) {
+        return null
+    }
+
+    let left = 0,
+        right = 0
+    while (right < nums.length) {
+        if (nums[right] !== 0) {
+            nums[left] = nums[right]
+            left++
+        }
+        right++
+    }
+
+    while (left < nums.length) {
+        if (nums[left] !== 0) {
+            nums[left] = 0
+        }
+        left++
+    }
+    return nums
 }
