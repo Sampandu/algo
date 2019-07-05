72e970eb246b00ba6e009b89d72e937a7b497643
Du <Sampan0103314@gmail.com>
2019-07-05 15:17:59 -0400

    solution2 to Leetcode 78

--- LeetCode/78-Subsets/solutions.js
+++ LeetCode/78-Subsets/solutions.js
@@ -3,6 +3,25 @@ var subsets = function(nums) {
   const results = [];
   if (nums === null) return results;
 
+  dfs(nums, 0, [], results);
+  return results;
+};
+
+function dfs(nums, index, arr, results) {
+  results.push(arr);
+
+  for (let i = index; i < nums.length; i++) {
+    arr.push(nums[i]);
+    dfs(nums, i + 1, [...arr], results);
+    arr.pop();
+  }
+}
+
+//solution2: DFS
+var subsets = function(nums) {
+  const results = [];
+  if (nums === null) return results;
+
   nums = nums.sort();
   dfs(nums, 0, [], results);
   return results;
