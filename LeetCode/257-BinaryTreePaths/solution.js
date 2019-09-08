2d65ef2e41e8b9f3b46fb33fd34bf684b4d10d11
Du <Sampan0103314@gmail.com>
2019-09-07 20:33:20 -0400

    Leetcode 257

---
+++ LeetCode/257-BinaryTreePaths/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+  if (root === null) return [];
+
+  const results = [];
+  findTreePaths(root, [], results);
+  return results;
+};
+
+function findTreePaths(n, temp, results) {
+  temp.push(n.val);
+
+  if (!n.left && !n.right) {
+    const path = temp.join('->');
+    temp = [];
+    results.push(path);
+    return;
+  }
+
+  n.left && findTreePaths(n.left, [...temp], results);
+  n.right && findTreePaths(n.right, [...temp], results);
+}
