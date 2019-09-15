0ffe081788adc625bbac1152361ec5a7f2c5f398
Du <Sampan0103314@gmail.com>
2019-09-15 10:25:35 -0400

    Leetcode 345

---
+++ LeetCode/345-ReverseVowelsOfaString/solution.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+
+// letter includes upper case and lower case letter
+var reverseVowels = function(s) {
+  if (s === null || s.length === 0) return '';
+
+  s = s.split('');
+  let start = 0,
+    end = s.length - 1;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  while (start < end) {
+    while (start < end && !vowels.includes(s[start])) {
+      start++;
+    }
+
+    while (start < end && !vowels.includes(s[end])) {
+      end--;
+    }
+
+    if (start < end) {
+      [s[start], s[end]] = [s[end], s[start]];
+      start++;
+      end--;
+    }
+  }
+  return s.join('');
+};
