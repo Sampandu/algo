9923746af9675b84150e53ebf8ead12338a2a325
Du <Sampan0103314@gmail.com>
2019-09-20 10:59:44 -0400

    modify the solution of leetcode 152

--- LeetCode/152-MaximumProductSubarray/solution.js
+++ LeetCode/152-MaximumProductSubarray/solution.js
@@ -21,3 +21,20 @@ var maxProduct = function(nums) {
 
   return res;
 };
+
+//solution
+var maxProduct = function(nums) {
+  if (nums === null || nums.length === 0) return;
+
+  let maxProd = nums[0],
+    minProd = nums[0];
+  let res = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    const prevMaxProd = maxProd;
+    maxProd = Math.max(prevMaxProd * nums[i], nums[i], minProd * nums[i]);
+    minProd = Math.min(prevMaxProd * nums[i], nums[i], minProd * nums[i]);
+    res = Math.max(maxProd, res);
+  }
+
+  return res;
+};
