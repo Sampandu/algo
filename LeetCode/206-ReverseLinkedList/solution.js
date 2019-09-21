ec106bae96125b770b379a8707cd761405d24d75
Du <Sampan0103314@gmail.com>
2019-09-21 14:30:50 -0400

    optimize the solution of Leetcode 206

--- LeetCode/206-ReverseLinkedList/solution.js
+++ LeetCode/206-ReverseLinkedList/solution.js
@@ -1,18 +1,33 @@
-//solution
+//solution 1
 const reverseList = head => {
   //check param
-  if(!head || !head.next) return head
+  if (!head || !head.next) return head;
 
-  let prev = head
-  let cur = prev.next
+  let prev = head;
+  let cur = prev.next;
   while (cur) {
-    let temp = cur.next
-    cur.next = prev
-    prev = cur
-    cur = temp
+    let temp = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = temp;
   }
-  head.next = null
-  head = prev
+  head.next = null;
+  head = prev;
 
-  return head
-}
+  return head;
+};
+
+//solution 2
+const reverseList = head => {
+  if (head === null) return null;
+
+  let prev = null,
+    cur = head;
+  while (cur !== null) {
+    const next = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = next;
+  }
+  return prev;
+};
---
+++ LeetCode/876-MiddleOfTheLinkedList/solution.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+  if (!head) return null;
+
+  let slow = head,
+    fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
