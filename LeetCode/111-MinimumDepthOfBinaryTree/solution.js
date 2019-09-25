39b1a1b0a2cea8a692e415626f2ad907c64575ce
Du <Sampan0103314@gmail.com>
2019-09-24 23:11:21 -0400

    Leetcode 111

--- LeetCode/111-MinimumDepthOfBinaryTree/solution.js
+++ LeetCode/111-MinimumDepthOfBinaryTree/solution.js
@@ -9,6 +9,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
+//solution1: top bottom
 var minDepth = function(root) {
   if (root === null) return 0;
 
@@ -28,3 +29,23 @@ function getMinDepth(n, previousDepth, minDepth) {
   n.left && getMinDepth(n.left, currentDepth, minDepth);
   n.right && getMinDepth(n.right, currentDepth, minDepth);
 }
+
+//solution2: bottom top
+var minDepth = root => {
+  if (root === null) return 0;
+
+  if (!root.left && !root.right) {
+    return 1;
+  }
+
+  let left = Infinity;
+  if (root.left) {
+    left = minDepth(root.left);
+  }
+  let right = Infinity;
+  if (root.right) {
+    right = minDepth(root.right);
+  }
+
+  return Math.min(left, right) + 1;
+};
