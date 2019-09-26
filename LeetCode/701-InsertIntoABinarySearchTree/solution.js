34306969dffccca4f1aab589d27bf4b8fa6b22b2
Du <Sampan0103314@gmail.com>
2019-09-26 12:03:49 -0400

    add one solution to Leetcode 701

--- LeetCode/701-InsertIntoABinarySearchTree/solution.js
+++ LeetCode/701-InsertIntoABinarySearchTree/solution.js
@@ -10,6 +10,7 @@
  * @param {number} val
  * @return {TreeNode}
  */
+//solution1: recursion, top bottom, tree level
 var insertIntoBST = function(root, val) {
   if (root === null || val === null) return;
 
@@ -28,3 +29,24 @@ function insert(root, val) {
     root.right = new TreeNode(val);
   }
 }
+
+//solution2: iteration
+var insertIntoBST = function(root, val) {
+  if (root === null || val === null) return;
+
+  let current = root;
+  while (current !== null) {
+    if (current.val > val && current.left) {
+      current = current.left;
+    } else if (current.val > val) {
+      current.left = new TreeNode(val);
+      break;
+    } else if (current.val < val && current.right) {
+      current = current.right;
+    } else if (current.val < val) {
+      current.right = new TreeNode(val);
+      break;
+    }
+  }
+  return root;
+};
