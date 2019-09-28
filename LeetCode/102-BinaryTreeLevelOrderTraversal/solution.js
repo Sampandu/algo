955da346b8dd25e31a6b9d4e8a9d5191a040f33a
Du <Sampan0103314@gmail.com>
2019-09-28 14:36:42 -0400

    Leetcode 102

---
+++ LeetCode/102-BinaryTreeLevelOrderTraversal/solution.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (root === null) return [];
+
+  const queue = [root];
+  const res = [];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      level.push(node.val);
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+    }
+    res.push(level);
+  }
+  return res;
+};
