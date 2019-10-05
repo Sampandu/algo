2c73a913eaf5de53edafa24f36fc04c770fb70e5
Du <Sampan0103314@gmail.com>
2019-10-05 10:35:07 -0400

    solution 2 for Leetcode 107

--- LCode/69-BinaryTreeLevelOrderTraversal/solution.js
+++ LCode/69-BinaryTreeLevelOrderTraversal/solution.js
@@ -1,58 +1,60 @@
 //solution1
-const levelOrder = function (root) {
-    // write your code here
-    if (root === null) {
-        return []
-    }
+const levelOrder = function(root) {
+  // write your code here
+  if (root === null) {
+    return [];
+  }
 
-    const queue = [root],
-          result = []
-    while (queue.length) {
-        const size = queue.length,
-             level = []
-        for (let i = 0; i < size; i++) {
-            const node = queue.shift()
-            if (node.left !== null) {
-                queue.push(node.left)
-            }
-            if (node.right !== null) {
-                queue.push(node.right)
-            }
-            level.push(node.val)
-        }
-        result.push(level)
+  const queue = [root],
+    result = [];
+  while (queue.length) {
+    const size = queue.length,
+      level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (node.left !== null) {
+        // pushing a node into the queue in terms of it is null or not, then we don't need to check if a node is null or not when unshifting a node from the queue
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+      level.push(node.val);
     }
-    return result
-}
+    result.push(level);
+  }
+  return result;
+};
 
 //solution2, use dummy node
 const levelOrder = root => {
   if (root === null) {
-    return
+    return;
   }
 
   const queue = [root, null],
-        result = []
-  let level = []
+    result = [];
+  let level = [];
   while (queue.length > 1) {
-    const node = queue.shift()
+    // > 1
+    const node = queue.shift();
 
     if (node === null) {
-      queue.push(null)
-      result.push(level)
-      level = []
-      continue
+      queue.push(null);
+      result.push(level);
+      level = [];
+      continue;
     }
 
-    level.push(node.val)
+    level.push(node.val);
     if (node.left !== null) {
-      queue.push(node.left)
+      queue.push(node.left);
     }
     if (node.right !== null) {
-      queue.push(node.right)
+      queue.push(node.right);
     }
   }
-  result.push(level)
+  result.push(level); //push level into result
 
-  return result
-}
+  return result;
+};
--- LeetCode/107-BinaryTreeLevelOrderTraversal-II/solution.js
+++ LeetCode/107-BinaryTreeLevelOrderTraversal-II/solution.js
@@ -9,29 +9,50 @@
  * @param {TreeNode} root
  * @return {number[][]}
  */
+//solution1
 var levelOrderBottom = function(root) {
   if (root === null) {
-      return []
+    return [];
   }
 
   const queue = [root],
-        res = []
+    res = [];
   while (queue.length) {
-      const size = queue.length
-      const subArr = []
+    const size = queue.length;
+    const subArr = [];
 
-      for (let i = 0; i < size; i++) {
-          const node = queue.shift()
-          if (node === null) {
-              continue
-          }
-          subArr.push(node.val)
-          queue.push(node.left)
-          queue.push(node.right)
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      if (node === null) {
+        continue;
       }
-      if (subArr.length !== 0) {
-          res.unshift(subArr)
-      }
+      subArr.push(node.val);
+      queue.push(node.left);
+      queue.push(node.right);
+    }
+    if (subArr.length !== 0) {
+      res.unshift(subArr);
+    }
   }
-  return res
+  return res;
+};
+
+//solution2: generic and clean BFS
+var levelOrderBottom = function(root) {
+  if (root === null) return [];
+
+  const queue = [root];
+  const res = [];
+  while (queue.length) {
+    const size = queue.length;
+    const level = [];
+    for (let i = 0; i < size; i++) {
+      const node = queue.shift();
+      node.left && queue.push(node.left);
+      node.right && queue.push(node.right);
+      level.push(node.val);
+    }
+    res.unshift(level);
+  }
+  return res;
 };
